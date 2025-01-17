77198f4e0bd703c0173bf5678ac0fa164987c571
justjinying <justjinying@gmail.com>
2025-01-17 11:24:33 +0800

    first commit

---
+++ src/main/java/com/jinying/springbootassignment/services/TaskService.java
@@ -0,0 +1,42 @@
+package com.jinying.springbootassignment.services;
+
+import com.jinying.springbootassignment.models.Task;
+import com.jinying.springbootassignment.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createNewTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTask() {
+        return taskRepository.findAll();
+    }
+
+    public Task findTaskById(Long id) {
+        return taskRepository.getById(id);
+    }
+
+    public List<Task> findAllCompletedTask() {
+        return taskRepository.findByCompletedTrue();
+    }
+
+    public List<Task> findAllInCompleteTask() {
+        return taskRepository.findByCompletedFalse();
+    }
+
+    public void deleteTask(Task task) {
+        taskRepository.delete(task);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
